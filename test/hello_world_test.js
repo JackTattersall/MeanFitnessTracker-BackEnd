ca421f41a40f5ff6af01cca4367fe181abdf5690
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-08-28 09:54:43 +0100

    adds mocha and chai and hello world test

---
+++ test/hello_world_test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const should = chai.should();
+
+describe('Hello world', () => {
+    it('Should be Hello World', () => {
+        should.equal('Hello world', 'Hello world')
+    })
+});
