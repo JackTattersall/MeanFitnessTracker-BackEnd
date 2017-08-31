d6c02559d25a74e7cce703de0cdc0238b1eb9a4c
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-08-31 19:42:23 +0100

    adds route for get user by id

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -8,6 +8,11 @@ module.exports = app => {
         .get(users.list_all_users)
         .post(users.create_a_user);
 
+    app.route('/users/:userId')
+        .get(users.find_user_by_id())
+        // .put(users.update_a_user)
+        // .delete(users.delete_a_user);
+
     app.route('/users/signin')
         .post(users.authenticate_a_user)
 };
